a4b3b57e0bb3460d3e783ebf1a26f13a84b02721
Youchan Jung <youchanj66@gmail.com>
2025-07-26 10:13:54 -0400

    entity

--- src/main/java/loopcampus/com/backend/entity/chat/ChatParticipant.java
+++ src/main/java/loopcampus/com/backend/entity/chat/ChatParticipant.java
@@ -3,6 +3,7 @@ package loopcampus.com.backend.entity.chat;
 
 import jakarta.persistence.*;
 import loopcampus.com.backend.entity.audit.Period;
+import loopcampus.com.backend.entity.member.Member;
 
 @Entity
 @Table(name = "chatParticipant")
@@ -10,4 +11,7 @@ public class ChatParticipant extends Period {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
 }
